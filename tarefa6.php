a5fb35effc59f0dfb9fa7d5fde622027dd236941
Fabio Andolphi <fabioandolphi@hotmail.com>
2021-07-16 18:29:49 -0300

    Refatoração - Reaprovetar o metodo toAmerican da classe MyDate

--- tarefa6.php
+++ tarefa6.php
@@ -1,25 +1,32 @@
 <?php
     class MyDate{
         
+        // metodo para transformar formato de data de brasileiro para americano. 
         public static function toAmerican($data){
             $data = str_replace("/","-",$data);
             $data = date('Y-m-d',  strtotime($data));
             return $data;
         }
 
+        // Metodo para transformar formato de data de americano para brasieliro.
+        public static function toBrazilian($data){
+            $data = date('d-m-Y',  strtotime($data));
+            $data = str_replace("-","/",$data);
+            return $data;
+        }
+
+        // Metodo para transformar formato de data de americano para brasileiro e vise versa. 
         public static function toggle($data){
             
             $padraoBR = "/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/";
             if(preg_match($padraoBR,$data)===1){
-                $data = str_replace("/","-",$data);
-                $data = date('Y-m-d',  strtotime($data));
+                $data = MyDate::toAmerican($data);
                 return $data;  
             }
 
             $padraoUSA = "/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/";
             if(preg_match($padraoUSA,$data)===1){
-                $data = date('d-m-Y',  strtotime($data));
-                $data = str_replace("-","/",$data);
+                $data = MyDate::toBrazilian($data);
                 return $data;
             }
 
@@ -29,8 +36,8 @@
         
     }
 
-    echo MyDate::toggle("2021-07-10")."<br>";
-    echo MyDate::toggle("10/07/2021")."<br>";
-    echo MyDate::toggle("10/07/21")."<br>";
+    echo "2021-07-10 = ".MyDate::toggle("2021-07-10")."<br>";
+    echo "10/07/2021 = ".MyDate::toggle("10/07/2021")."<br>";
+    echo "10/07/21 = ".MyDate::toggle("10/07/21")."<br>";
 
 // Não fechei a tag php propositalmente, por conta de ser uma boa prática de programação, e muitos frameworks fazem o mesmo. 
