cf4087ac8e69dca52d051e79c7dff3470847c911
Fabio Andolphi <fabioandolphi@hotmail.com>
2021-07-17 11:26:04 -0300

    Acerto da tarefa 1, retirar o uso de count()

--- tarefa1.php
+++ tarefa1.php
@@ -1,8 +1,10 @@
 <?php
     $valores = array(1,3,5,9,12,10);
     $soma = 0;
-    for($x=0;$x<count($valores);$x++){
-        $soma += $valores[$x];
+    foreach ($valores as $valor){
+        $soma += $valor;
     }
     echo $soma;
+
+
 // Não fechei a tag php propositalmente, por conta de ser uma boa prática de programação, e muitos frameworks fazem o mesmo. 
